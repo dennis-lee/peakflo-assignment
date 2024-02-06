b2e881b5f98b14e0c450fa0451bba37b82ac4a76
Dennis Lee <dennis.lee@xendit.co>
2024-02-06 21:37:17 +0800

    chore: setup typeorm to generate migrations

---
+++ ormconfig.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm'
+import 'dotenv/config'
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  migrations: ['./migrations/*.ts'],
+  entities: ['./src/repositories/entities/*Entity.ts'],
+})
